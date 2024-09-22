4a4416cb7ea4be57159e717b2ef8592e4d9d83b4
Kane <44578147+kingchou007@users.noreply.github.com>
2024-09-22 21:53:48 +0800

    [update] scripts, fix dataset loading error

--- python_lib/README.md
+++ python_lib/README.md
@@ -4,8 +4,15 @@ download dataset
 mkdir data
 ```
 
-download [gustil](https://cloud.google.com/storage/docs/gsutil_install)
+install [gustil](https://cloud.google.com/storage/docs/gsutil_install), and download the dataset
 
 ```
 gsutil -m cp -r gs://gresearch/robotics/droid_100 ./data
 ```
+
+
+
+
+<img src="obs_example.gif" alt="Demo" width="30%">
+
+<img src="traj_example.gif" alt="Demo" width="30%">
--- python_lib/visualize_traj.py
+++ python_lib/visualize_traj.py
@@ -1,36 +1,20 @@
-import os
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.transform import Rotation as R
+from PIL import Image
 import tensorflow_datasets as tfds
+import cv2
 from termcolor import cprint
 
 class TrajectoryPlotter:
-    def __init__(self, data_path):
-        self.data_path = data_path
-        self.colors = plt.cm.jet(np.linspace(0, 1, 30))
-
-    def load_demo_data(self, demo_dir, step_size=10):
-        demo_files = os.listdir(demo_dir)
-        demo_files.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
-        
-        positions = []
-        rotations = []
-
-        for step_idx in range(0, len(demo_files), step_size):
-            demo = np.load(os.path.join(demo_dir, demo_files[step_idx]), allow_pickle=True)
-            step_data = demo['arm_ee_pose']
-            positions.append(step_data[:3])
-            rotations.append(step_data[3:])
-
-        return positions, rotations
+    def __init__(self, colors=None):
+        self.colors = colors or plt.cm.jet(np.linspace(0, 1, 30))
 
     def plot_trajectory(self, ax, positions, rotations, color):
         x, y, z = zip(*positions)
         roll, pitch, yaw = zip(*rotations)
 
-        ax.plot(x, y, z, marker='o', color=color)
+        ax.plot(x, y, z, marker='o')
 
         for i in range(len(x)):
             pos = np.array([x[i], y[i], z[i]])
@@ -38,49 +22,72 @@ class TrajectoryPlotter:
             rot_matrix = r.as_matrix()
             for j in range(3):
                 start = pos
-                end = pos + 0.1 * rot_matrix[:, j]
+                end = pos + 0 * rot_matrix[:, j]
                 ax.plot([start[0], end[0]], [start[1], end[1]], [start[2], end[2]], color=color)
 
-    def plot_trajectories(self):
-        data = os.listdir(self.data_path)
-        data.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
+    def plot_trajectory_from_dict(self, trajectory_dict, fig_size=(10, 8)):
+        positions, rotations = zip(*[(step_data[:3], step_data[3:]) for step_data in trajectory_dict])
 
-        fig = plt.figure(figsize=(10, 8))
-        ax = fig.add_subplot(111, projection='3d')
+        fig, ax = plt.subplots(figsize=fig_size, subplot_kw={'projection': '3d'})
+        color = self.colors[0]
 
-        for demo_idx, demo_name in enumerate(data[:30]):
-            demo_dir = os.path.join(self.data_path, demo_name)
-            positions, rotations = self.load_demo_data(demo_dir)
-            color = self.colors[demo_idx]
-            self.plot_trajectory(ax, positions, rotations, color)
-            ax.text(positions[-1][0], positions[-1][1], positions[-1][2], f'Trajectory {demo_idx+1}', color=color)
+        traj_images = []
+        for i in range(len(positions)):
+            ax.clear()
+            self.plot_trajectory(ax, positions[:i+1], rotations[:i+1], color)
+            ax.set(title='End-Effector Trajectory with Rotation in 3D Space', xlabel='X', ylabel='Y', zlabel='Z')
 
-        ax.set_title('End-Effector Trajectories with Rotation in 3D Space')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
+            fig.canvas.draw()
+            image = Image.frombytes('RGB', fig.canvas.get_width_height(), fig.canvas.tostring_rgb())
+            traj_images.append(image)
 
-        save_path = os.path.join('trajectories.png')
-        plt.savefig(save_path)
-        print(f'Saved {save_path}')
-        plt.show()
+        plt.close(fig)
+        return traj_images
+
+def load_dataset(data, step_size=1):
+    return zip(*[(data[i][:3], data[i][3:]) for i in range(0, len(data), step_size)])
+
+def save_video(images, path="trajectory.mp4", fps=15):
+    height, width, _ = np.array(images[0]).shape
+    video_writer = cv2.VideoWriter(path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+
+    for image in images:
+        video_writer.write(cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
+
+    video_writer.release()
 
 def main():
+    #TODO: modify here to the local dataset path
     builder = tfds.builder_from_directory(builder_dir="python_lib/data/droid_100/1.0.0")
     builder.info.features
-    cprint(builder.info.features, "green")
+    cprint(builder.info.features)
 
-    ds = tfds.load("droid_100", data_dir="python_lib/data/", split="train")
-    
-    
+    #TODO: modify here to the local dataset path
+    ds = tfds.load("droid_100", data_dir="python_lib/data", split="train")
 
-    
+    images = []
+    cat_pose = []
+    for episode in ds.shuffle(10, seed=0).take(1):
+        for step in episode["steps"]:
+            cat_pose.append(step["action_dict"]["cartesian_position"].numpy())
+            images.append(
+                Image.fromarray(
+                    np.concatenate((
+                        step["observation"]["exterior_image_1_left"].numpy(),
+                        step["observation"]["exterior_image_2_left"].numpy(),
+                        step["observation"]["wrist_image_left"].numpy(),
+                    ), axis=1)
+                )
+            )
 
-    # open the dataset
+    pos, rot = load_dataset(cat_pose)
+    trajectory_dict = [np.concatenate((p, r)) for p, r in zip(pos, rot)]
 
-    # data_path = ''
-    # plotter = TrajectoryPlotter(data_path)
-    # plotter.plot_trajectories()
+    plotter = TrajectoryPlotter()
+    traj_images = plotter.plot_trajectory_from_dict(trajectory_dict)
+
+    # display.Image(as_gif(images))
+    save_video(traj_images)
 
 if __name__ == '__main__':
     main()
