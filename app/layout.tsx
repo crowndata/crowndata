f3c6f3cd1938761215ae73d5c0f727a2a1363eb8
Wei Ji <jiwei0706@gmail.com>
2024-09-28 11:10:02 -0700

    add dual compare

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Header from "components/Header";
-import "./globals.css";
+import Header from "@/components/Header";
+import "@/styles/globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
