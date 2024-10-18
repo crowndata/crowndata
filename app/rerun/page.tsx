c10e8ceb9157ed7f1f60ad25fdf8a033ae27f92f
Wei Ji <jiwei0706@gmail.com>
2024-10-18 13:04:23 -0700

    refactor rerun

--- app/rerun/page.tsx
+++ app/rerun/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import WebViewer from "@rerun-io/web-viewer-react";
 import { Suspense } from "react";
+
+import RerunPage from "@/components/RerunPage";
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading data...</div>}>
-      <WebViewer
-        width="800px"
-        height="600px"
-        rrd="https://app.rerun.io/version/0.19.0/examples/dna.rrd"
-      />
+      <RerunPage />
     </Suspense>
   );
 }
