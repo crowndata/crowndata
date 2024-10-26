c57fec6901d52b981f2623fed49af5332d6fd049
Wei Ji <jiwei0706@gmail.com>
2024-10-25 22:40:21 -0700

    rename

--- utils/geometry.ts
+++ utils/geometry.ts
@@ -5,7 +5,7 @@ type URDFFile = {
 export const assemblyURDFFiles: URDFFile = {
   DROID: "/geometries/DROID/panda.urdf",
   rm_65_gazebo_dual_gripper:
-    "/geometries/ros2_rm_robot/rm_description/urdf/rm_65_gazebo_dual_gripper.urdf",
+    "/geometries/ros2_rm_robot/rm_description/urdf/rm_65_gazebo_dual.urdf",
 };
 
 export const armURDFFiles: URDFFile = {
